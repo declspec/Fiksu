3acf0fec4d94abc1061252ecb96fb8a357abd072
Jason Larke <jason.larke@curtin.edu.au>
2020-10-29 16:34:58 +0800

    Updated DatabaseMigrator interface to return the number of migrations applied or rolled back

--- src/Omnis.Database/IDatabaseMigrator.cs
+++ src/Omnis.Database/IDatabaseMigrator.cs
@@ -1,6 +1,15 @@
 ﻿namespace Omnis.Database {
     public interface IDatabaseMigrator {
-        void MigrateTo(long version);
-        void MigrateToLatest();
+        /// <summary>
+        /// Migrate the database up or down to a particular version
+        /// </summary>
+        /// <param name="version">The desired version to migrate to</param>
+        /// <returns>The total number of migrations applied or rolled back (negative value for rollbacks)</returns>
+        int MigrateTo(long version);
+        /// <summary>
+        /// Migrate the database up to the latest version
+        /// </summary>
+        /// <returns>The total number of migrations applied</returns>
+        int MigrateToLatest();
     }
 }
